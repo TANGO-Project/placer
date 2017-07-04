62d98c996d1eeb44ff8c95b7f27d4e35214200c8
Renaud De Landtsheer <renaud.delandtsheer@cetic.be>
2017-07-04 10:32:41 +0200

    added copyright and ref to TANGO

--- src/main/scala/placerT/Main.scala
+++ src/main/scala/placerT/Main.scala
@@ -1,4 +1,7 @@
 /*
+ * Copyright 2017 CETIC www.cetic.be
+ * This is being developed for the TANGO Project: http://tango-project.eu
+ *
  * Placer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
